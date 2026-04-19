1eb3136c82765fa1f4d9714b555bc87a08077250
Nikita3890 <nkondrusenkov@gmail.com>
2026-04-19 17:39:19 +0500

    fourth

--- cmd/mane.py
+++ cmd/mane.py
@@ -1,16 +1,29 @@
+import os
 import requests
+from bs4 import BeautifulSoup
+
 URL_1 = 'https://www.python.org/'
 URL_2 = 'https://www.wikipedia.org/'
 def main():
     req = requests.get(URL_2, headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36'})
     if req.status_code == 200:
         print(req.headers)
-        print(req.text)
-        with open('req.html', 'wb') as f:
-            f.write(req.content)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        lang_list = soup.find_all('a', class_='link-box')
+        result_path = '../results'
+        if not os.path.exists(result_path):
+            os.makedirs(result_path)
+        with open(f'{result_path}/wikipedia-result.txt', 'w', encoding='utf-8') as f:
+            for element in lang_list:
+                url = f"https:{element['href']}"
+                lang = element.find('strong').text
+                cnt = element.find('small').text
+                print(lang, url, cnt)
+                f.write(f'{url};{lang};{cnt}\n')
+        
+
     else:
         print(f'Ошибка запроса\n Status Code:{req.status_code}')
-        with open('req.html', 'wb') as f:
-            f.write(req.content)
+
 if __name__ == '__main__':
     main()
